0c0ada454f481e56e3ee27784ab65ad5ed5d62b5
giuseppemazze0 <a22204542@alunos.ulht.pt>
2025-12-02 16:49:04 +0000

    input melhorad

--- components/Input/Input.tsx
+++ components/Input/Input.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { count } from "console"
+import { convertSegmentPathToStaticExportFilename } from "next/dist/shared/lib/segment-cache/segment-value-encoding"
 import { useEffect, useState } from "react"
 
 
@@ -38,50 +38,55 @@ export default function Input() {
     const [visivel, setVisivel] = useState<boolean>(false)
     const [idTarefa, setIdTarefa] = useState<number>(tarefas.length)
 
-    function alterarCorFundo(e) {
+    function definirCorFundo(valor:string, cor: string) {
+        setVisivel(true)
+        setCorFundo(cor)
+        setOpcao(valor)
+    }
+
+    function alterarCorFundo(e: any) {
         const valor = e.target.value
+
+        console.log()
 
         switch(valor) {
             case 'tecnologia':
-                setVisivel(true)
-                setCorFundo('bg-purple-500')
-                setOpcao(valor.toUpperCase())
+                definirCorFundo(valor, 'bg-purple-500')
                 break
 
             case 'categorias':
-                setVisivel(true)
-                setCorFundo('bg-orange-300')
-                setOpcao(valor.toUpperCase())
+                definirCorFundo(valor, 'bg-orange-300')
                 break
 
             default:
+                setOpcao("")
                 setVisivel(false)
         }
     }
 
     function desenharElementoTarefa(visivel: boolean, opcao: string, corFundo: string, texto: string, editor:boolean, id: number) {
-    return (
-        <div className={`relative overflow-hidden ${visivel ? 'flex' : 'hidden'} flex-col justify-between items-center w-[260px] h-[120px] pb-[25px] mx-auto ${corFundo}`}>
-            <h3 className="w-full p-[6px] border-b-[2px] border-black text-center font-bold">
-                {opcao}
-            </h3>
-            <p>
-                {texto}
-            </p>
-            <div className={`absolute top-[3px] left-[50%] ${editor ? 'flex' : 'hidden'} flex-row-reverse gap-x-[140px] transform -translate-x-[50%]`}>
-                <button onClick={() => apagarTarefa(id)} className="cursor-pointer p-[3px] rounded-full bg-red-500 hover:bg-red-600 active:bg-red-800">
-                    <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="black"><path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"/></svg>
-                </button>
-                <button className="cursor-pointer p-[3px] rounded-full bg-pink-200 hover:bg-pink-300 active:bg-pink-500">
-                    <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="black"><path d="M200-200h57l391-391-57-57-391 391v57Zm-80 80v-170l528-527q12-11 26.5-17t30.5-6q16 0 31 6t26 18l55 56q12 11 17.5 26t5.5 30q0 16-5.5 30.5T817-647L290-120H120Zm640-584-56-56 56 56Zm-141 85-28-29 57 57-29-28Z"/></svg>
-                </button>
+        return (
+            <div key={`tarefa-${id}`} className={`relative overflow-hidden ${visivel ? 'flex' : 'hidden'} flex-col justify-between items-center w-[260px] h-[120px] pb-[25px] mx-auto ${corFundo}`}>
+                <h3 className="w-full p-[6px] border-b-[2px] border-black text-center font-bold">
+                    {opcao.toUpperCase()}
+                </h3>
+                <p>
+                    {texto}
+                </p>
+                <div className={`absolute top-[3px] left-[50%] ${editor ? 'flex' : 'hidden'} flex-row-reverse gap-x-[140px] transform -translate-x-[50%]`}>
+                    <button onClick={() => apagarTarefa(id)} className="cursor-pointer p-[3px] rounded-full bg-red-500 hover:bg-red-600 active:bg-red-800">
+                        <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="black"><path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"/></svg>
+                    </button>
+                    <button onClick={() => editarTarefa(id)} className="cursor-pointer p-[3px] rounded-full bg-pink-200 hover:bg-pink-300 active:bg-pink-500">
+                        <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="black"><path d="M200-200h57l391-391-57-57-391 391v57Zm-80 80v-170l528-527q12-11 26.5-17t30.5-6q16 0 31 6t26 18l55 56q12 11 17.5 26t5.5 30q0 16-5.5 30.5T817-647L290-120H120Zm640-584-56-56 56 56Zm-141 85-28-29 57 57-29-28Z"/></svg>
+                    </button>
+                </div>
+                {fazerEfeitosNasPontas()}
             </div>
-            {fazerEfeitosNasPontas()}
-        </div>
-    )
-}
+        )
+    }
     function adicionarTarefa() {
-        if (!visivel) {
+        if (!visivel || !texto.trim()) {
             return
         }
 
@@ -96,6 +101,9 @@ export default function Input() {
                 corFundo: corFundo
             }
         ])
+        setTexto("")
+        setOpcao("")
+        setVisivel(false)
     }
     function apagarTarefa(id: number) {
         setIdTarefa(id-1)
@@ -104,12 +112,25 @@ export default function Input() {
             _.filter((tarefa) => tarefa.id !== id)
         ))
     }
+    function editarTarefa(id: number) {
+        const tarefaAtual = tarefas.find((tarefa) => tarefa.id === id)
+        
+        if (tarefaAtual) {
+            setTexto(tarefaAtual.texto)
+            setOpcao(tarefaAtual.opcao.toLowerCase())
+            setIdTarefa(tarefaAtual.id)
+            setCorFundo(tarefaAtual.corFundo)
+            setVisivel(true)
+        }
+
+        console.log(tarefaAtual)
+        apagarTarefa(id)
+    }
 
 
 
     useEffect(() => {
         localStorage.setItem('tarefas', JSON.stringify(tarefas))
-        console.log(idTarefa)
     }, [tarefas])
 
 
@@ -120,13 +141,13 @@ export default function Input() {
                     <label htmlFor="campoTexto">
                         Escreva algo no campo abaixo
                     </label>
-                    <input onChange={(e) => setTexto(e.target.value)} id="campoTexto" type="text" maxLength={28} className="w-[300px] h-[50px] border-[2px] text-[.9em]"/>
+                    <input onChange={(e) => setTexto(e.target.value)} value={texto} id="campoTexto" type="text" maxLength={28} className="w-[300px] h-[50px] border-[2px] text-[.9em]"/>
                 </div>
 
                 <div className="flex flex-col gap-y-[20px]">
                     <div>
                         <label htmlFor="TecCat">Lista de</label>
-                        <select onChange={alterarCorFundo} id="TecCat" className="cursor-pointer">
+                        <select onChange={(e) => alterarCorFundo(e)} value={opcao} id="TecCat" className="cursor-pointer">
                             <option value="">...</option>
                             <option value="tecnologia">Tecnologia</option>
                             <option value="categorias">Categorias</option>
