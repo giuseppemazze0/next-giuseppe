1e9274b6b0306924f47ad65b21aeb8ef3939405f
giuseppemazze0 <a22204542@alunos.ulht.pt>
2025-12-12 11:20:09 +0000

    .

--- components/Produtos/DetailsCard.tsx
+++ components/Produtos/DetailsCard.tsx
@@ -2,22 +2,63 @@
 
 import { useParams } from "next/navigation"
 import useSWR from "swr"
+import Image from "next/image"
+import Link from "next/link"
 import { Produto } from "@/models/interfaces"
 
 
 
-const params = useParams()
-const index = Number(params.produto)
-const url_produto = `https://deisishop.pythonanywhere.com//products/${index}`
-
 const fetcher = async (url: string) => fetch(url).then(res => res.json())
 
 export default function DetailsCard() {
-    const {produto, error, isLoading} = useSWR<Produto>(url_produto, fetcher)
+    const params = useParams()
+    const index = Number(params.produto) + 1
+    const url = 'https://deisishop.pythonanywhere.com'
+    const url_produto = `${url}/products/${index}`
+
+    const {data, error, isLoading} = useSWR<Produto>(url_produto, fetcher)
+
+    if (error) return <p>Erro ao carregar</p>;
+    if (isLoading) 
+        return (
+            <>
+                <p>
+                    Carregando...
+                </p>
+            </>
+        );
+    if (!data) return <p>Dados inexistente</p>;
 
     return (
-        <>
-            { produto }
-        </>
+        <div>
+            <Link href='/produtos'>
+                voltaa
+            </Link>
+
+            <h2 className="font-bold text-[2em] text-center">
+                { data.title }
+            </h2>
+
+            <p>
+                { data.category }
+            </p>
+
+            <figure className="relative flex items-center w-full h-[250px]">
+                <Image src={`${data.image}`} alt={`Imagem do produto '${data.title}'`} fill style={{objectFit: 'contain'}}></Image>
+            </figure>
+
+            <p>
+                { data.rating.rate } 
+                { data.rating.count }
+            </p>
+
+            <p className="indent-[20px]">
+                { data.description }
+            </p>
+
+            <p className="">
+                { Number(data.price).toFixed(2) } €
+            </p>
+        </div>
     )
 }
